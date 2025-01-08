15bdda6ed3433a5a8ae8abcd22c94d53a586c888
Akash Lakshmanan <akash.lak2006@gmail.com>
2025-01-08 17:01:23 -0500

    Create HistogramofTweets.sql

---
+++ DataLemur/HistogramofTweets.sql
@@ -0,0 +1,9 @@
+WITH total_tweets AS (
+  SELECT COUNT(tweet_id) AS tweet_bucket, user_id
+  FROM tweets
+  WHERE EXTRACT(YEAR FROM tweet_date) = '2022'
+  GROUP BY user_id
+)
+SELECT tweet_bucket, COUNT(user_id) AS users_num
+FROM total_tweets
+GROUP BY tweet_bucket
