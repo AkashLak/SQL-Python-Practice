e7c2742d92f827b17d5d9294d24dcd9e00d86090
Akash Lakshmanan <akash.lak2006@gmail.com>
2025-01-08 17:08:06 -0500

    Update ProductSalesAnalysisIII.sql

--- LeetCode/ProductSalesAnalysisIII.sql
+++ LeetCode/ProductSalesAnalysisIII.sql
@@ -3,3 +3,10 @@ WITH smallest_year AS (
    FROM Sales
    GROUP BY product_id
 )
+
+
+SELECT s.product_id, first_year, quantity, price
+FROM Sales s
+JOIN smallest_year sy
+ON s.product_id = sy.product_id AND s.year = sy.first_year
+
